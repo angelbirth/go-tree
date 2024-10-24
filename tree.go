e9931b78f7dff7f00aaf883ed21a499a4c94ab05
Engelbert <ricky.ason@gmail.com>
2024-10-24 16:02:36 +0700

    initial commit

---
+++ tree.go
@@ -0,0 +1,69 @@
+package tree
+
+func NewTree[T comparable]() *Tree[T] {
+	return &Tree[T]{
+		isRoot: true,
+	}
+}
+
+type Tree[T comparable] struct {
+	children []*Tree[T]
+	val      T
+	isRoot   bool
+}
+
+func (t *Tree[T]) Search(x T) *Tree[T] {
+	if t.val == x && !t.isRoot {
+		return t
+	}
+	return t.searchChildren(x)
+}
+
+func (t *Tree[T]) searchChildren(x T) *Tree[T] {
+	for _, child := range t.children {
+		if child.val == x {
+			return child
+		} else if c := child.searchChildren(x); c != nil {
+			return c
+		}
+	}
+	return nil
+}
+
+func (t *Tree[T]) AddChild(x T) *Tree[T] {
+	c := &Tree[T]{
+		val: x,
+	}
+	t.children = append(t.children, c)
+	return c
+}
+
+func (t *Tree[T]) AddChildren(x ...T) []*Tree[T] {
+	var res []*Tree[T]
+	for _, n := range x {
+		c := &Tree[T]{
+			val: n,
+		}
+		t.children = append(t.children, c)
+		res = append(res, c)
+	}
+	return res
+}
+
+func (t *Tree[T]) PreorderTraverse(f func(*Tree[T])) {
+	if !t.isRoot {
+		f(t)
+	}
+	for _, child := range t.children {
+		child.PreorderTraverse(f)
+	}
+}
+
+func (t *Tree[T]) PostorderTraverse(f func(*Tree[T])) {
+	for _, child := range t.children {
+		child.PostorderTraverse(f)
+	}
+	if !t.isRoot {
+		f(t)
+	}
+}
---
+++ tree_test.go
@@ -0,0 +1,93 @@
+package tree
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTree_Search(t *testing.T) {
+	var tests = []struct {
+		desc  string
+		init  func(*Tree[int])
+		x     int
+		found bool
+	}{
+		{
+			desc:  "search empty tree",
+			x:     1,
+			found: false,
+		},
+		{
+			desc: "search 1-child tree",
+			x:    1,
+			init: func(t *Tree[int]) {
+				t.AddChild(1)
+			},
+			found: true,
+		},
+		{
+			desc: "search in the middle of the tree",
+			x:    4,
+			init: func(t *Tree[int]) {
+				t.AddChild(1).AddChild(2).AddChild(4).AddChild(8)
+			},
+			found: true,
+		}, {
+			desc: "search nonexisting value",
+			x:    3,
+			init: func(t *Tree[int]) {
+				t.AddChild(1).AddChild(2).AddChild(4)
+			},
+			found: false,
+		}, {
+			desc: "search with >1 children, >1 depth",
+			x:    5,
+			init: func(t *Tree[int]) {
+				node16 := t.AddChild(1).AddChild(2).AddChild(4).AddChild(8).AddChild(16)
+				node16.AddChild(5)
+				node16.AddChild(32)
+			},
+			found: true,
+		},
+		{
+			desc: "search with >1 children, >1 depth, nonexisting value",
+			x:    6,
+			init: func(t *Tree[int]) {
+				node16 := t.AddChild(1).AddChild(2).AddChild(4).AddChild(8).AddChild(16)
+				node16.AddChild(5)
+				node16.AddChild(32)
+			},
+			found: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			tree := new(Tree[int])
+			if tt.init != nil {
+				tt.init(tree)
+			}
+			assert.Equal(t, tt.found, tree.Search(tt.x) != nil)
+		})
+	}
+
+}
+
+func TestTree_Traverse(t *testing.T) {
+	tree := NewTree[rune]()
+	f := tree.AddChild('F')
+	b := f.AddChild('B')
+	g := f.AddChild('G')
+	b.AddChild('A')
+	d := b.AddChild('D')
+	d.AddChildren('C', 'E')
+	g.AddChild('I').AddChild('H')
+	tree.PreorderTraverse(func(t *Tree[rune]) {
+		fmt.Printf("%c ", t.val)
+	})
+	fmt.Println()
+	tree.PostorderTraverse(func(t *Tree[rune]) {
+		fmt.Printf("%c ", t.val)
+	})
+}
